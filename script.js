2f0267c199a18f215c8ae6b935a46749ee8c3e43
DEIVA SIVAM <a.s.deiva@gmail.com>
2023-01-06 15:09:58 +0530

    Js Added

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+let myText = document.getElementById("eveluatedText");
+myText.addEventListener("input", () => {
+    let count = (myText.value).length;
+document.getElementById("letterCount").textContent = `${count}`;
+});
